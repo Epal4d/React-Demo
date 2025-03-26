8657c25b1167ced7e47a7a48c5c55c20e77bc175
evan.al4d <evan.al4d@gmail.com>
2025-03-26 12:13:32 -0500

    Add Home Page

---
+++ react-demo-app/src/components/Home.js
@@ -0,0 +1,62 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+function App() {
+  
+    //functions 
+    function lnkdemo1()
+    {
+      let msgText = "# App:: lnkdemo1"
+      console.log(msgText)
+    }
+  
+    //es5+ arrow functions - optimized functions 
+    const lnkdemo2 = () =>
+    {
+      let msgText = "# App:: lnkdemo1"
+      console.log(msgText)
+    }
+  
+    const btndemo1 = () =>
+    {
+      let msgText = "# App:: btndemo1"
+      console.log(msgText)
+    }
+  
+    //components - jsx - function retun(<> ..jsx.. </>)
+    //- add props
+    //- external file - export/import 
+
+    <>
+      <div>
+          <Header/>
+          <p></p>
+          <a href="#" onClick={lnkdemo1}>link demo 1</a>
+          <p></p>
+          <a href="#" onClick={lnkdemo2}>link demo 1</a>
+          <p></p>
+          <button onClick={btndemo1}>button demo 1</button>
+          <p></p>
+          <p>
+            Footer @ 2025
+          </p>
+      </div>
+    </>
+  
+    return (
+      <>
+        <div>
+            <Header/>
+            <p></p>
+            <a href="#" onClick={lnkdemo1}>link demo 1</a>
+            <p></p>
+            <a href="#" onClick={lnkdemo2}>link demo 1</a>
+            <p></p>
+            <button onClick={btndemo1}>button demo 1</button>
+            <p></p>
+            <Footer title="footer" year={2025}></Footer>
+        </div>
+      </>
+  );
+}
+  
+export default App;
